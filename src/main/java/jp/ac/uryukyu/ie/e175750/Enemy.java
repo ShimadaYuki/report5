1f6cf887ded9a7265b896e07643c14ccb0d58034
Shimada Yuki <e175750@ie.u-ryukyu.ac.jp>
2017-12-11 17:17:01 +0900

    second commit

--- src/main/java/jp/ac/uryukyu/ie/e175750/Enemy.java
+++ src/main/java/jp/ac/uryukyu/ie/e175750/Enemy.java
@@ -4,4 +4,27 @@ public class Enemy extends LivingThing {
     public Enemy(String name, int maximumHP, int attack) {
         super(name, maximumHP, attack);
     }
-}
+    @Override
+        public void attack(LivingThing opponent) {
+            int damage = (int) (Math.random() * getAttack());
+            int random = (int)(Math.random()*100);
+            if (random<40) {
+            if (damage == 0) {
+                    System.out.printf("%sの攻撃！会心の一撃！！,,,だが、%sは攻撃を回避した！\n", getName(), opponent.getName(), damage);
+                }else{
+                    System.out.printf("%sの攻撃！会心の一撃！！%sに%dのダメージを与えた！！\n", getName(), opponent.getName(), damage*2);
+                    opponent.wounded(damage * 2);
+                }
+            }else{
+                if (isDead() == false) {
+                    if (damage == 0) {
+                        System.out.printf("%sの攻撃！,,,だが、%sは攻撃を回避した！\n", getName(), opponent.getName(), damage);
+                    }else{
+                        System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", getName(), opponent.getName(), damage);
+                        opponent.wounded(damage);
+                    }
+                }
+            }
+        }
+    }
+
